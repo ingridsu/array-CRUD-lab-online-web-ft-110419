398634ffec10b37bba4c49d8703771b496efc972
Ingrid sura <sura.ingrid@gmail.com>
2019-11-18 15:44:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
 def create_an_empty_array
-  
+   []
 end
 
 def create_an_array
-  
+pets = ["cat", "dog", "mouse", "snake"]
 end
 
 def add_element_to_end_of_array(array, element)
+
   
 end
+puts add_element_to_start_of_array
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(pets)
+puts pets = ["cat", "dog", "mouse", "snake"]
+    array.unshift ("gerbal")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  pets = ["cat", "dog", "mouse", "snake"]
+  snake = pets.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+   remove_element_from_start_of_array
+   array = pets.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+   array []
 end
 
 def retrieve_first_element_from_array(array)
-  
+ aray = [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+   array= [3]
 end
